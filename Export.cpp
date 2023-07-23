4e0d8fc45b74a50b6357b31aadf318ded0ee3eab
Junjie <djjbxxz@gmail.com>
2023-07-23 13:45:17 -0400

    update

--- Export.cpp
+++ Export.cpp
@@ -14,6 +14,21 @@ PYBIND11_MODULE(gen_colorline_data_pytorch, m)
 	m.def("_994_to_9928", &_994_to_9928, "123");
 	m.def("rule", &rule, "123");
 
+	py::class_<Game_map>(m, "Game_map", pybind11::buffer_protocol())
+		//.def(py::init<>())
+		.def_readwrite("game_map", &Game_map::_data)
+		.def_readwrite("coming_chess", &Game_map::coming_chess);
+			
+		//.def_property("v1", [](Game_map& self) {
+		//return py::array<char>(self._data.size(), self._data.data());
+		//	}, [](Game_map& self, py::array_t<char> new_val) {
+		//		std::copy_n(new_val.data(), self.v1.size(), self.v1.data());
+		//	})
+		//.def_property("v2", [](Game_map& self) {
+		//		return py::array_t<char>(self.v2.size(), self.v2.data());
+		//	}, [](Game_map& self, py::array_t<char> new_val) {
+		//		std::copy_n(new_val.data(), self.v2.size(), self.v2.data());
+		//	});
 
 	pybind11::class_<Game_map>(m, "Game_map", pybind11::buffer_protocol())
 		.def_buffer([](Game_map& m) -> pybind11::buffer_info {
@@ -23,7 +38,7 @@ PYBIND11_MODULE(gen_colorline_data_pytorch, m)
 			pybind11::format_descriptor<char>::format(),	/* Python struct-style format descriptor */
 			1,												/* Number of dimensions */
 			{ BOARD_SIZE* BOARD_SIZE + COMING_CHESS_NUM },						/* Buffer dimensions */
-			{ sizeof(char) * (BOARD_SIZE * BOARD_SIZE + COMING_CHESS_NUM)}   /* Strides (in bytes) for each index */
+			{ sizeof(char)}   /* Strides (in bytes) for each index */
 		);
 
 			}).def_readwrite("board", &Game_map::_data)
@@ -41,18 +56,18 @@ PYBIND11_MODULE(gen_colorline_data_pytorch, m)
 		);
 			});
 
-	pybind11::class_<Color>(m, "Color", pybind11::buffer_protocol())
-		.def_buffer([](Color& m) -> pybind11::buffer_info {
-		return pybind11::buffer_info(
-			&m.value,								/* Pointer to buffer */
-			sizeof(char),									/* Size of one scalar */
-			pybind11::format_descriptor<char>::format(),	/* Python struct-style format descriptor */
-			1,												/* Number of dimensions */
-			{ 1 },						/* Buffer dimensions */
-			{ sizeof(char) }   /* Strides (in bytes) for each index */
-		);
+	//pybind11::class_<Color>(m, "Color", pybind11::buffer_protocol())
+	//	.def_buffer([](Color& m) -> pybind11::buffer_info {
+	//	return pybind11::buffer_info(
+	//		&m.value,								/* Pointer to buffer */
+	//		sizeof(char),									/* Size of one scalar */
+	//		pybind11::format_descriptor<char>::format(),	/* Python struct-style format descriptor */
+	//		1,												/* Number of dimensions */
+	//		{ 1 },						/* Buffer dimensions */
+	//		{ sizeof(char) }   /* Strides (in bytes) for each index */
+	//	);
 
-			});
+	//		});
 
 	pybind11::class_<game_statu_9928>(m, "game_statu_9928", pybind11::buffer_protocol())
 		.def_buffer([](game_statu_9928& m) -> pybind11::buffer_info {
